882f34f3c9e4a52224bd90250259ecdceed94d50
Adriano Regis <adbrum@outlook.com>
2021-08-25 21:35:34 +0100

    created global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
 export function App() {
   return (
     <div className="App">
-      <Title>Main</Title>
+      <h1>Main</h1>
+      <GlobalStyle />
     </div>
   );
 }
